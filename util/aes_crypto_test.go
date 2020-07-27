8283dfb4f96924f6592121d040664a9c57bdb43c
mengrenxiong@gmail.com <mengrenxiong@gmail.com>
2020-07-28 01:57:55 +0800

    feat: 添加 消息加密 函数

--- util/aes_crypto_test.go
+++ util/aes_crypto_test.go
@@ -2,25 +2,27 @@ package util
 
 import (
 	"encoding/base64"
-	"fmt"
 	"testing"
 )
 
-func TestAesDecryptMessage(t *testing.T) {
-	aesKey, err := base64.StdEncoding.DecodeString("HogNecGqZeDxFIDGjBwWKw==")
-	aesIv, err := base64.StdEncoding.DecodeString("aoZqkfGDWwqj6rFgWdafyw==")
+func TestAesEncryptMessage(t *testing.T) {
+	appId := "wx3808f4e98b0e3a57"
+	aesKey, err := base64.StdEncoding.DecodeString("itZDdxGBnFRaBPmFD12CcYalbatKW1fLtuIfQw8iQ57=")
 
-	base64Test := "4B9B1aknFM6yQjAh9mFxH3iN4PZXUGfpBLB98CRAVZzNfUI4J1WUur70+NSH/5MXcCidrt44hi6dkByTtRPxrTIV1BOOTvaa2G5NWunTXZJ37/Oq0ezydbDal5v+X3bvVVeFR6MkhYI+hT9xVl2XnE/Bzon2gIq9F9Fy8Yny0VPqsQ95xUyXnN3/IuhiquR1pAgKjDK3kgCoqhUVNa0dRRQQgTNIpy1djbLfyErPXGTXe1qhAj7RvDdJtRloEfg63JgaB/QTR2BLEGT7/GfHnwROfngxa3esGDeBr9Mtav67R4PjYESVFtH2Yf2npaDWAvAMvr+8hNVd2tjy/3HgImctZWo7bh1OHa/ktH4wKYVbTgxZPg/lgTWC+zsl1Z9g07vWQyGpaA11HODDGn1Kdvh6esiY7T3JQ15nKs1rdyXigNQFb9+kicPiInKVfOiuMcGEazli5/UQHF5rnuWhkg/wy+PRbZybR18lLh5d+EW1CnK8gNcQblDJPvx6QFcFhPxr28WkEd7ys0BZQGCIkQ=="
-	ciphertext, err := base64.StdEncoding.DecodeString(base64Test)
 	if err != nil {
-		t.Error(err)
+		t.Error("base64 error")
 		return
 	}
 
-	raw, err := AesDecryptMessage(ciphertext, aesKey, aesIv)
-	fmt.Println(string(raw), err)
-	if err != nil {
-		t.Error(err)
-		return
+	random := []byte("1234567890")
+	plainText := []byte("<xml><ToUserName><![CDATA[gh_815dcdde3c52]]></ToUserName>\n<FromUserName><![CDATA[fastwego]]></FromUserName>\n<CreateTime>1595869404</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[tests_message]]></Content>\n<MsgId>1234567</MsgId>\n</xml>")
+
+	SuccessBase64CipherText := "70nIsuWJcN2F4vcDSXJrRbs1q9h+FbMrgoEKP3q30K4CWX9OiCrEDBZtXb0w7RneUVYq8OHo3Q492jU8Zw8mDw9dPTFjLkfxXiQinzIAtXLiOblGmrZtDWseKcg7HvHMqHzb0gnf4lG9J1eO7KY5HnanX7OB7c4TOg53Q2LYPqLOLlLV9wpBSoL9+kKwNPTMJNB1DCpai9+I+Fh4kUIwQok84Bf1PNNEStRqlxlh7tLNOeScd4UmGWi8U2rQNvcfANyuQixy1fIHz5WVRIbTGMB8uUF4j4mhbATqPOaxv+fQxgeE7zutvOmBE2mjIzmBwcoSZHxQAyyGsQQo52E1pqfuUJgr15zepEdC2+qopQdYbKkonOMzSxG36DDsGbmrmW53BRXKvtgHDeR7qdmW7kG407zPI/7Yrg+kKCqlNVY="
+	cipherText := AesEncryptMessage(random, plainText, appId, aesKey)
+	base64CipherText := base64.StdEncoding.EncodeToString(cipherText)
+
+	if base64CipherText != SuccessBase64CipherText {
+		t.Error("test TestAesEncryptMessage fail")
 	}
+	return
 }
