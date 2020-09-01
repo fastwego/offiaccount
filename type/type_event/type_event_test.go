d5911e7c781d4c231ce0fa2640da25c267085943
Ceelog <chenwei@ceelog.org>
2020-09-01 20:12:19 +0800

    update test case

--- type/type_event/type_event_test.go
+++ type/type_event/type_event_test.go
@@ -2,30 +2,35 @@ package type_event
 
 import (
 	"encoding/xml"
-	"fmt"
 	"testing"
 )
 
 func TestEventMenuPicSysPhoto(t *testing.T) {
-	s := `<xml><ToUserName><![CDATA[gh_e136c6e50636]]></ToUserName>
-<FromUserName><![CDATA[oMgHVjngRipVsoxg6TuX3vz6glDg]]></FromUserName>
-<CreateTime>1408090651</CreateTime>
-<MsgType><![CDATA[event]]></MsgType>
-<Event><![CDATA[pic_sysphoto]]></Event>
-<EventKey><![CDATA[6]]></EventKey>
-<SendPicsInfo><Count>3</Count>
-<PicList>
-<item><PicMd5Sum><![CDATA[1b5f7c23b5bf75682a53e7b6d163e185]]></PicMd5Sum></item>
-<item><PicMd5Sum><![CDATA[2b5f7c23b5bf75682a53e7b6d163e185]]></PicMd5Sum></item>
-<item><PicMd5Sum><![CDATA[3b5f7c23b5bf75682a53e7b6d163e185]]></PicMd5Sum></item>
-</PicList>
-</SendPicsInfo>
-</xml>`
+	s := `
+	<xml>
+		<ToUserName><![CDATA[gh_e136c6e50636]]></ToUserName>
+		<FromUserName><![CDATA[oMgHVjngRipVsoxg6TuX3vz6glDg]]></FromUserName>
+		<CreateTime>1408090651</CreateTime>
+		<MsgType><![CDATA[event]]></MsgType>
+		<Event><![CDATA[pic_sysphoto]]></Event>
+		<EventKey><![CDATA[6]]></EventKey>
+		<SendPicsInfo>
+			<Count>3</Count>
+			<PicList>
+				<item><PicMd5Sum><![CDATA[1b5f7c23b5bf75682a53e7b6d163e185]]></PicMd5Sum></item>
+				<item><PicMd5Sum><![CDATA[2b5f7c23b5bf75682a53e7b6d163e185]]></PicMd5Sum></item>
+				<item><PicMd5Sum><![CDATA[3b5f7c23b5bf75682a53e7b6d163e185]]></PicMd5Sum></item>
+			</PicList>
+		</SendPicsInfo>
+	</xml>`
 
 	photo := EventMenuPicSysPhoto{}
 	err := xml.Unmarshal([]byte(s), &photo)
 	if err != nil {
-		t.Fatalf("xml parser error=%+v\n", err)
+		t.Errorf("xml parser error=%+v\n", err)
 	}
-	fmt.Printf("photo=%+v\n", photo)
+
+	if len(photo.SendPicsInfo.PicList.Item) != 3 {
+		t.Errorf("xml parser error %+v\n", photo)
+	}
 }
